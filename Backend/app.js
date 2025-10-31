ba39d8cc4b5fbc477d38dea74929576c05b3bb98
firafis <firafisberhanu4@gmail.com>
2025-10-31 04:01:11 +0300

    fix

--- Backend/app.js
+++ Backend/app.js
@@ -58,7 +58,7 @@ const path = require("path");
 const router = require("./routes/route");
 const app = express();
 
-app.use(cors({ origin: process.env.FRONTEND_URL || "https://stock-management-system-ie-network-zeta.vercel.app", credentials: true }));
+app.use(cors({ origin: process.env.FRONTEND_URL || "https://stock-management-system-ie-network-ten.vercel.app", credentials: true }));
 // app.use(cors({ origin: process.env.FRONTEND_URL || " http://localhost:5173/", credentials: true }));  for local purpose
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
