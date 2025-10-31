82973851614bcf33655ba17e636e3cebb61d4756
firafis <firafisberhanu4@gmail.com>
2025-10-31 03:01:34 +0300

    adding the render database url

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -2,7 +2,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  // baseURL: 'http://localhost:3000/api', for local development
+  baseURL: 'https://file-for-render-1.onrender.com',
+
 });
 
 export default api
