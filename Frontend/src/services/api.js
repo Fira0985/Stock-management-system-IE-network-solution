c5fd777dc23c91bc87250a8350546cc3594941b4
firafis <firafisberhanu4@gmail.com>
2025-10-31 03:02:16 +0300

    updating the backend url

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL: 'http://localhost:3000/api', for local development
-  baseURL: 'https://file-for-render-1.onrender.com',
+  baseURL: 'https://stock-management-system-ie-network.vercel.app',
 
 });
 
